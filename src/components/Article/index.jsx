0efbb693b63a5bdb8888e31dfb8dfe78988e3b60
steven <info@cnlselectronics.co.uk>
2022-04-23 18:50:28 +0100

    anything

--- src/components/Article/index.jsx
+++ src/components/Article/index.jsx
@@ -7,16 +7,16 @@ const Article = () => {
     <article className="mt-90">
   <header className="text-center mb-40">
     <h3>
-      <a href="/article/revivalhistory">The History of the Revival dApp</a>
+     The History of the Revival dApp
     </h3>
     <div className="link-color-default fs-5">
       <a href="#">News, </a>
       <time>April 23, 2022</time>
     </div>
   </header>
-  <a href="/article/revivalhistory">
-    <img className="rounded" src="assets/img/socialbanner.jpeg" target="article/revivalhistory"  alt="/article/revivalhistory" />
-  </a>
+
+    <img className="rounded" src="assets/img/socialbanner.jpeg"  />
+  
   <div className="card-block">
     <p className="text-justify">Although not originally planned, staking in the Revival dApp has been wildly popular with our holders. Yes, you read that correctly, staking pools and even the dApp itself were not part of the original RevivalDeFi plan.</p>
     <p className="text-center mt-40">
